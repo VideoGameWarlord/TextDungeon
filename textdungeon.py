cb9b23b08c3ce723fad8af031489b8054cbe06af
VideoGameWarlord <videogamewarlord@gmail.com>
2023-11-28 16:08:43 -0600

    Update textdungeon.py

--- textdungeon.py
+++ textdungeon.py
@@ -6,154 +6,247 @@
 import pickle
 import random
 
-enemy = {
-    'name': 'Goblin',
-    'health': 20,
-    'attack': 8,
-    'defense': 2,
-    'alive': True
-}
-
 # Define an NPC with a quest
 npc = {
     'name': 'Old Wizard',
-    'dialogue': 'Greetings, traveler! The cursed goblins have stolen my magic scroll. Can you retrieve it for me?',
+    'dialogue':
+    'Greetings, traveler! The cursed goblins have stolen my magic scroll. Can you retrieve it for me?',
     'quest': {
         'description': 'Retrieve the magic scroll from the goblins.',
         'completed': False
     },
     'reward': {
-        'xp': 100,
-        'items': [{'name': 'Magic Wand', 'type': 'attack', 'value': 5, 'cost': 100}],
+        'xp':
+        100,
+        'items': [{'name': 'Magic Wand', 'type': 'attack', 'value': 5, 'cost': 100 }],
         'gold': 50
     }
 }
 
-# Example items, try adding more items!
-health_potion = {'name': 'Health Potion', 'type': 'heal', 'value': 20, 'cost': 30}
+# Example items
+health_potion = {
+    'name': 'Health Potion',
+    'type': 'heal',
+    'value': 20,
+    'cost': 30
+}
+attack_boost = {
+    'name': 'Attack Boost',
+    'type': 'attack',
+    'value': 5,
+    'cost': 50
+}
 
-# Shop inventory, try creating a shop room! (Hint: still a 'key' : value pair)
+# Shop inventory
 shop_inventory = [health_potion, attack_boost]
+# To add to inventory
+#player['inventory'].append(health_potion)
 
-# Define a dictionary of rooms in the game, each with descriptions and exits
-# Use a flow chart site like https://www.lucidchart.com/ to have a clear idea of your rooms layout
+# This is the rooms which contrains a dictorinary of different rooms, with
+# descriptions and enemies in each room. The exits are also stored in a list
+# which is linked to the room dict.
 rooms = {
     'start': {
-        'description': 'You are in the starting room. There\'s a door to the north.',
-        'exits': {'north': 'hallway'}
+        'description':
+        'You are in the starting room. There\'s a door to the north.',
+        'exits': {
+            'north': 'hallway'
+        }
     },
     'hallway': {
         'description':
         'You are in a long hallway. There are doors to the north and south.',
         'exits': {
             'south': 'start',
-            'north': 'hallwaytwo'
+            'north': 'shop',
+            'east': 'wizard_hut',
+            'west': 'library'
         },
-        'enemy': enemy
+        'enemy': {
+          'name': 'Goblin',
+          'health': 5,
+          'attack': 8,
+          'defense': 2,
+          'alive': True
+      }
     },
     # Define a room with an NPC
-    rooms['wizard_hut'] = {
+    'wizard_hut': {
         'description': 'An old wizard appears to be in deep thought here.',
+        'exits': {
+            'west': 'hallway',
+        },
         'npc': npc
         # Add in exits
+    },
+      'library': {
+        'description': 'Rows upon rows of ancient books surround you.',
+        'exits': {
+            'east': 'hallway',
+        },
+        'enemy': {
+            'name': 'Cursed Librarian',
+            'health': 60,
+            'attack': 12,
+            'defense': 8,
+            'alive': True
+        }
+    },
+    'shop': {
+        'description':
+        'You are in a shop. There are doors to the north and south.',
+        'exits': {
+            'south': 'hallway',
+            'north': 'hallwaytwo'
+        },
+        'inventory': shop_inventory
     },
     'hallwaytwo': {
         'description':
         'You are in a long hallway. There are doors to the north and south.',
         'exits': {
-            'south': 'hallway',
-            'north': 'treasure',
+            'south': 'shop',
+            'north': 'guarded_treasury',
+            'east': 'crypt'
         },
-        # Another way to implement enemies into the game without declaring a variable earlier
         'enemy': {
-          'name': 'Orc',
-          'health': 30,
-          'attack': 10,
-          'defense': 2,
-          'alive': True
-      }
+            'name': 'Orc',
+            'health': 30,
+            'attack': 10,
+            'defense': 2,
+            'alive': True
+        }
+    },
+    'crypt': {
+        'description':
+        'A chilling wind greets you in this dark crypt. Beware of the undead!',
+        'exits': {
+            'west': 'hallwaytwo'
+        },
+        'enemy': {
+            'name': 'Phantom',
+            'health': 80,
+            'attack': 15,
+            'defense': 5,
+            'alive': True
+        }
+    },
+    'closet': {
+        'description':
+        'You are in a long hallway. There is a door to the north.',
+        'exits': {
+            'north': 'hallwaytwo'
+        },
+    },
+    'guarded_treasury': {
+        'description':
+        'Gold and jewels shine brightly, but a dragon guards this room!',
+        'exits': {
+            'north': 'treasure',
+            'south': 'hallwaytwo'
+        },
+        'enemy': {
+            'name': 'Dragon',
+            'health': 150,
+            'attack': 25,
+            'defense': 10,
+            'alive': True
+        }
     },
     'treasure': {
         'description': 'Congratulations! You found the treasure room!',
-        'exits': {'south': 'hallway'}
+        'exits': {
+            'south': 'guarded_treasury'
+        }
     },
 }
 
-# Combat function that takes player and enemy dictionaries as parameters
+
 def combat(player, enemy):
-# The fight continues as long as both have more than 0 health
   while player['health'] > 0 and enemy['health'] > 0:
-      # Ask the player for their action
-      action = input('Do you want to "attack" or "defend"? ')
+    action = input('Do you want to "attack", "defend", or "run"? ')
 
-      # If the player chooses to attack, calculate damage and update the enemy's health
-      if action == 'attack':
-          # We use the max() function in case the player's attack is less than the enemy's defense.
-          damage = max(0, player['attack'] - enemy['defense'])
-          # Deal damage to the enemy's health
-          enemy['health'] -= damage
-          print(f"You dealt {damage} damage to the {enemy['name']}!")
+    if action == 'attack' or action == 'a':
+      damage = max(0, player['attack'] - enemy['defense'])
+      enemy['health'] -= damage
+      print(f"You dealt {damage} damage to the {enemy['name']}!")
 
-          # Check if the enemy has been defeated
-          if enemy['health'] <= 0:
-              enemy['alive'] = False
-              print(f"You defeated the {enemy['name']}!")
-              #Add to XP however much XP this enemy is worth
-              check_level_up(player)
-              break # Exit the combat loop if the enemy is defeated
+      if enemy['health'] <= 0:
+        enemy['alive'] = False
+        print(f"You defeated the {enemy['name']}!")
+        # Loot logic
+        loot_gold = 20  # or some logic to determine loot amount
+        player['gold'] += loot_gold
+        print(f"You found {loot_gold} gold on the enemy.")
 
-      # If the player didn't choose to defend, calculate the full damage from the enemy's attack
-      if action != 'defend':
-          player_damage = max(0, enemy['attack'] - player['defense'])
-      # If the player chooses to defend, reduce the damage taken by half
-      else:
-          player_damage = max(0, enemy['attack'] // 2)
+        # Chance to drop an item
+        if random.random() < 1.0:  # 30% chance to drop an item
+          loot_item = {
+              'name': 'Healing Herb',
+              'type': 'heal',
+              'value': 10,
+              'cost': 10
+          }
+          player['inventory'].append(loot_item)
+          print(f"The enemy dropped a {loot_item['name']}.")
 
-      # Update the player's health after taking damage
-      player['health'] -= player_damage
-      print(f"The {enemy['name']} dealt {player_damage} damage to you!")
+        player['xp'] += 50  # or however much XP this enemy is worth
+        print(f"You gained {player['xp']} XP.")
+        check_level_up(player)
+        break
 
-      # Check if the player has been defeated
-      if player['health'] <= 0:
-          print("You have been defeated!")
-          break # Exit the combat loop if the player is defeated
+    if action != 'defend':
+      player_damage = max(0, enemy['attack'] - player['defense'])
+    else:
+      player_damage = max(0, enemy['attack'] // 2)
 
-      # Show current health statuses after the round of combat
-      health_status = f"Your health: {player['health']},"
-      enemy_status = f"{enemy['name']} health: {enemy['health']}"
-      print(health_status, enemy_status)
-      
-# Levels the player up and increases their stats
+    player['health'] -= player_damage
+    print(f"The {enemy['name']} dealt {player_damage} damage to you!")
+
+    if action == 'run':
+      print("You managed to escape!")
+      break
+
+    if player['health'] <= 0:
+      print("You have been defeated!")
+      break
+
+    health_status = f"Your health: {player['health']},"
+    enemy_status = f"{enemy['name']} health: {enemy['health']}"
+    print(health_status, enemy_status)
+
+
 def check_level_up(player):
   if player['xp'] >= player['xp_to_level']:
     player['level'] += 1
     player['xp'] = 0  # Reset XP
     player['xp_to_level'] *= 2  # Increase XP needed for next level
-    player['health'] += 20 # Increase player stats
+    player['health'] += 20
     player['attack'] += 5
-    player['defense'] += 5
     print(f"You've leveled up to level {player['level']}!")
+
 
 player = {}
 
 # The initialize_player function should create a new player dictionary with default values
 def initialize_player():
-  # Set up the default player data, a dictionary representing the player's attributes
+  # Set up the default player data
   return {
       'health': 100,
       'attack': 10,
       'defense': 5,
-      # Try adding in XP and levels! Will also need a key with the value being the amount of XP needed to level up.
+      'level': 1,
+      'xp': 0,
+      'xp_to_level': 100,
       'gold': 100,
-      'inventory': [], #Contains a list of items
+      'inventory': [],
       'attack_boost_duration': 0
   }
 
 def initialize_game():
   action = input('Would you like to "load" a game or "start" a new one? ')
   if action == 'load':
-    return load_game(
-    )  # This will either return a valid game state or (None, '')
+    return load_game()  # This will either return a valid game state or (None, '')
   else:
     # If starting a new game or if loading failed, initialize player
     print('Starting a new game...')
@@ -165,7 +258,7 @@ file_path = 'textrpg.pickle'
 def save_game(player, current_room):
   try:
     with open(file_path, 'wb') as file:
-      game_state = {'player': player, 'current_room': current_room}
+      game_state = {'player': player, 'current_room': current_room, 'rooms' : rooms}
       pickle.dump(game_state, file)
       print('Game saved succesfully!')
   except Exception as e:
@@ -178,10 +271,12 @@ def load_game():
       game_state = pickle.load(file)
       player = game_state['player']
       current_room = game_state['current_room']
+      rooms = game_state['rooms']
       return player, current_room
   except FileNotFoundError:
     print("No saved game found.")
     return None, ''
+
 
 # Initialize the game (either load a saved game or start a new one)
 player, current_room = initialize_game()
@@ -190,58 +285,100 @@ player, current_room = initialize_game()
 if player is None:
   player = initialize_player()  # Make sure this function returns a valid player dictionary
   current_room = 'start'
+  #rooms = initialize_rooms()  # Function to create the initial rooms
+#else:
+    # Use the loaded rooms if available
+    #rooms = loaded_rooms if loaded_rooms else initialize_rooms()
 
-# Start the main game loop
+def display_player_stats():
+  print(f"Level: {player['level']}")
+  print(f"XP: {player['xp']}/{player['xp_to_level']}")
+  print(f"Health: {player['health']}")
+  print(f"Attack: {player['attack']}")
+  print(f"Defense: {player['defense']}")
+  print(f"Gold: {player['gold']}")
+  print(f"Inventory: {player['inventory']}")
+
+def display_inventory():
+  if len(player['inventory']) == 0:
+    print("Your inventory is empty.")
+  else:
+    print("Your inventory:")
+    
+
+# Main Game Loop
 while True:
-    # Print the description of the current room
-    print(rooms[current_room]['description'])
+  print(rooms[current_room]['description'])
 
-    # Todo: Add shop logic
+  # Print exits of the current room
+  print("Exits:")
 
-    # Check if there is an NPC in the room
-    if 'npc' in rooms[current_room]:
-        print(f"You see {rooms[current_room]['npc']['name']}.")
-        action = input('Do you want to "talk" to the NPC or "leave"? ')
+  # Check if there is an NPC in the room
+  if 'npc' in rooms[current_room]:
+    print(f"You see {rooms[current_room]['npc']['name']}.")
+    # After enemy defeat
+    if 'quest' in player and not player['quest']['completed']:
+      # Check if the conditions are met (e.g., a specific enemy was defeated)
+      player['quest']['completed'] = True
+      print("You have completed the quest!")
 
-        # After enemy defeat
-        if 'quest' in player and not player['quest']['completed']:
-            # Check if the conditions are met (e.g., a specific enemy was defeated)
-            player['quest']['completed'] = True
-            print("You have completed the quest!")
-            
-            # Grant quest rewards
-            player['xp'] += npc['reward']['xp']
-            player['gold'] += npc['reward']['gold']
-            player['inventory'].extend(npc['reward']['items'])
-        
-        if action == 'talk':
-            print(rooms[current_room]['npc']['dialogue'])
-            quest_action = input('Will you "accept" the quest? (yes/no) ')
-            if quest_action == 'yes':
-                player['quest'] = rooms[current_room]['npc']['quest']
-                print(f"Quest accepted: {player['quest']['description']}")
+      # Grant quest rewards
+      player['xp'] += npc['reward']['xp']
+      player['gold'] += npc['reward']['gold']
+      player['inventory'].extend(npc['reward']['items'])
 
-    #Check if there's an enemy in the current room and if it's alive
-    if 'enemy' in rooms[current_room] and rooms[current_room]['enemy']['alive']:
-        print(f"You see a {rooms[current_room]['enemy']['name']}!")
-        combat(player, rooms[current_room]['enemy']) # Start combat function
-        if player['health'] <= 0:
-            print("Game Over!")  #End the game if the player has been defeated
-            break
-            
-    # Ask the player what they want to do next      
-    action = input('Which direction will you go? (or type "quit" to exit) ')
+    action = input('Do you want to "talk" to the NPC or "leave"? ')
 
-    # Move to the next room if the direction is valid
-    if action in rooms[current_room]['exits']:
-        current_room = rooms[current_room]['exits'][action]
-    elif action == 'quit':
-        print('Goodbye!') # End the game if the player decides to quit
-        break
-    # Handle the case where the player inputs an invalid direction
-    else:
-        print('Invalid direction.')
+    if action == 'talk':
+      print(rooms[current_room]['npc']['dialogue'])
+      quest_action = input('Will you "accept" the quest? (yes/no) ')
+      if quest_action == 'yes':
+        player['quest'] = rooms[current_room]['npc']['quest']
+        print(f"Quest accepted: {player['quest']['description']}")
 
-    # Instead of picking a direction, just type 'save' to save the game.
-    if (action == 'save'):
-        save_game(player, current_room)
+  # If the player is in the shop
+  if current_room == 'shop':
+    print("Items available:")
+    for idx, item in enumerate(rooms['shop']['inventory']):
+      print(f"{idx+1}. {item['name']} - {item['cost']} gold")
+
+    action = input('Do you want to "buy [item number]" or "leave"? ')
+
+    if action.startswith('buy'):
+      item_idx = int(action.split(' ')[1]) - 1  # Convert user input to item index
+      item_to_buy = rooms['shop']['inventory'][item_idx]
+
+      if player['gold'] >= item_to_buy['cost']:
+        player['gold'] -= item_to_buy['cost']
+        player['inventory'].append(item_to_buy)
+        print(f"You bought {item_to_buy['name']}!")
+      else:
+        print("You don't have enough gold.")
+
+  # Check for enemy encounter
+  if 'enemy' in rooms[current_room] and rooms[current_room]['enemy']['alive']:
+    print(f"You see a {rooms[current_room]['enemy']['name']}!")
+    # Pass in player and enemy in the room we are currently in.
+    combat(player, rooms[current_room]['enemy'])
+    if player['health'] <= 0:
+      print("Game Over!")
+      break
+
+  action = input('Which direction will you go? (or type "quit" to exit) ')
+
+  if action in rooms[current_room]['exits']:
+    current_room = rooms[current_room]['exits'][action]
+  elif action == 'quit':
+    print('Goodbye!')
+    break
+  else:
+    print('Invalid direction.')
+
+  if (action == 'save'):
+    save_game(player, current_room)
+
+  if(action == 'check stats'):
+    display_player_stats()
+
+  if(action == 'check inv'):
+    display_inventory()
